63a64e310c1db510ae40069e09675b99821dda41
jaewook <eari100@naver.com>
2026-07-19 18:24:28 +0900

    bind, listen

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ class IocpServer
         // 서버 초기화 및 리슨 소켓 생성
         bool Start()
         {
-            // 윈도우 커널에 고성능 IOCP포트 개설 요청
+            // 윈도우 커널에 고성능 IOCP포트 개설 요청 (4th param=0: CPU core 자동 할당)
             m_hIOCP = CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
             if (m_hIOCP == NULL) 
             {
@@ -46,6 +46,7 @@ class IocpServer
 			}
 
             // Async Overlapped IO TCP Socket 개방
+			// AF_INET: IPv4, SOCK_STREAM: TCP, 0: default protocol, WSA_FLAG_OVERLAPPED: 비동기 IO
             m_hListenSock = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
             if (m_hListenSock == INVALID_SOCKET)
             {
@@ -53,7 +54,31 @@ class IocpServer
                 return false;
             }
 
-            std::cout << "IOCP 포트 및 중첩 IO 소켓 생성 완료.\n";
+            // 주소 구조체 설정
+			SOCKADDR_IN serverAddr;
+            // 0으로 초기화
+			ZeroMemory(&serverAddr, sizeof(serverAddr));
+            // IPv4 주소 체계
+			serverAddr.sin_family = AF_INET;
+            // 내 컴퓨터의 모든 랜카드 IP 주소 수용
+			serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+			serverAddr.sin_port = htons(m_port);
+
+            // bind: 소켓에 주소 할당
+            if (::bind(m_hListenSock, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+            {
+                std::cerr << "Bind err\n";
+                return false;
+			}
+
+            // listen: 대기실 개방   
+			// SOMAXCONN: 커널이 허용하는 최대 대기열 길이의 대기 큐(backlog) 생성
+            if (::listen(m_hListenSock, SOMAXCONN) == SOCKET_ERROR)
+            {
+                std::cerr << "socket listen err\n";
+            }
+
+            std::cout << "IOCP 포트 및 중첩 IO 소켓 생성 완료 포트: " << m_port << '\n';
 
             return true;
         }
